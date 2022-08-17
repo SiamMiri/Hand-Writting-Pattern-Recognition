b14236e2a0b0baa91383e92ad02002cec52d19e2
studmirisi2219 <studmirisi2219@stud.th-rosenheim.de>
2022-08-17 20:57:20 +0200

    KNN Network

---
+++ KNN/include/knn.h
@@ -0,0 +1,14 @@
+//
+// Created by siamakmirifar on 16.08.22.
+//
+
+#ifndef HANDWRITTINGDETECTION_KNN_H
+#define HANDWRITTINGDETECTION_KNN_H
+
+
+class knn {
+
+};
+
+
+#endif //HANDWRITTINGDETECTION_KNN_H
---
+++ KNN/src/knn.cpp
@@ -0,0 +1,5 @@
+//
+// Created by siamakmirifar on 16.08.22.
+//
+
+#include "knn.h"
